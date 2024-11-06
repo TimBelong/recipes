597b547e28ca30cd53f463cf1f24c7a3f362b881
inne <mrbelongtim@gmail.com>
2024-11-06 19:17:11 +0100

    fixes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,15 +3,15 @@ import logo from '../assets/logo.png';
 
 const Header = () => {
 	return (
-		<div className="px-96 py-5 flex items-center justify-between">
+		<header className="px-4 md:px-16 lg:px-32 xl:px-80 py-5 flex items-center justify-between">
 			<a href="/"><img src={logo} alt="" className="header_logo"/></a>
 
-			<ul className='nav flex items-center justify-between gap-10'>
+			<ul className='nav hidden md:flex items-center justify-between gap-10'>
 				<li className="text-2xl"><a href="#">Блюдо дня</a></li>
 				<li className="text-2xl"><a href="#">Калории</a></li>
 				<li className="text-2xl"><a href="#">Добавление</a></li>
 			</ul>
-		</div>
+		</header>
 	);
 };
 
--- src/Components/Meals.jsx
+++ src/Components/Meals.jsx
@@ -20,7 +20,7 @@ const Meals = () => {
 		});
 	}
 	return (
-		<div className="px-96 py-8">
+		<div className="px-4 md:px-16 lg:px-32 xl:px-80 py-8">
 			<button
 				onClick={getRandomMeals}
 				className="mb-8 px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600"
@@ -30,7 +30,7 @@ const Meals = () => {
 			{randomMeals && (
 				<div className="mb-8">
 					<h2 className="text-2xl font-bold mb-4">Ваши блюда на сегодня</h2>
-					<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+					<div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
 						<MealItem meal={randomMeals.breakfast} />
 						<MealItem meal={randomMeals.lunch} />
 						<MealItem meal={randomMeals.dinner} />
